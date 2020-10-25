18f24f535007ca289b04f7963cfa5f8328de62c0
Yogesh <user@Kedars-MacBook-Air.local>
2020-10-25 11:03:47 +0530

    config folder gitignore

---
+++ app/config/index.js
@@ -0,0 +1,9 @@
+const PORT = 3000;
+const MONGO_DB_URI = 'mongodb://admin:admin1234@ds111489.mlab.com:11489/indusgeek';
+const JWT_SECRET_KEY = 'thisisasamplesecret';
+
+module.exports = {
+    PORT,
+    MONGO_DB_URI,
+    JWT_SECRET_KEY
+}
