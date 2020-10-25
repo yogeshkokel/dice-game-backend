2ca6da72210d0e668a25634a21e8a90c9adf5f60
Yogesh <user@Kedars-MacBook-Air.local>
2020-10-25 11:27:05 +0530

    env variable

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 const config = require('./app/config');
-
-const PORT = config.PORT || 3000;
+require('dotenv').config({ path: __dirname + '/.env' })
+const PORT = process.env.PORT || 80;
 
 const server = http.createServer(app);
 
